755b6d0b64457c41b4028601970c669ecf987c98
Ava Wandersleben <78870469+AvaWandersleben@users.noreply.github.com>
2026-04-24 16:00:35 -0400

    Add files via upload

--- final_project.h
+++ final_project.h
@@ -2,7 +2,9 @@
 #define FINALPROJECT
 
 #include <Toggle.h>
-#include <Stepper.h>
+#include <math.h>
+#include <TimeLib.h>
+#include <ResponsiveAnalogRead.h>
 
 #include <Audio.h>
 #include <Wire.h>
@@ -19,26 +21,27 @@
 #include "AudioSampleC2.h"
 
 // GUItool: begin automatically generated code
-AudioPlayMemory          playMem6;       //xy=142,555
-AudioPlayMemory          playMem7;       //xy=147,618
-AudioPlayMemory          playMem1;       //xy=161,140
-AudioPlayMemory          playMem4;       //xy=162,392
-AudioPlayMemory          playMem2;       //xy=169,214
-AudioPlayMemory          playMem5;       //xy=174,474
-AudioPlayMemory          playMem8;       //xy=180,682
-AudioPlayMemory          playMem3;       //xy=186,309
-AudioEffectFade          fade1;          //xy=308,145
-AudioEffectFade          fade2;          //xy=345,207
-AudioEffectFade          fade3;          //xy=361,300
-AudioEffectFade          fade4;          //xy=392,387
-AudioEffectFade          fade7;          //xy=393,625
-AudioEffectFade          fade6;          //xy=406,558
-AudioEffectFade          fade5;          //xy=414,464
-AudioEffectFade          fade8;          //xy=417,679
-AudioMixer4              mixer1;         //xy=621,245
-AudioMixer4              mixer2;         //xy=620,547
-AudioMixer4              mixer3;         //xy=801,387
-AudioOutputI2S           i2s1;           //xy=1118,412
+AudioPlayMemory          playMem6;       //xy=116,458
+AudioPlayMemory          playMem7;       //xy=121,521
+AudioPlayMemory          playMem1;       //xy=135,43
+AudioPlayMemory          playMem4;       //xy=136,295
+AudioPlayMemory          playMem2;       //xy=143,117
+AudioPlayMemory          playMem5;       //xy=148,377
+AudioPlayMemory          playMem8;       //xy=154,585
+AudioPlayMemory          playMem3;       //xy=160,212
+AudioEffectFade          fade1;          //xy=282,48
+AudioEffectFade          fade2;          //xy=319,110
+AudioEffectFade          fade3;          //xy=335,203
+AudioEffectFade          fade4;          //xy=366,290
+AudioEffectFade          fade7;          //xy=367,528
+AudioEffectFade          fade6;          //xy=380,461
+AudioEffectFade          fade5;          //xy=388,367
+AudioEffectFade          fade8;          //xy=391,582
+AudioMixer4              mixer1;         //xy=595,148
+AudioMixer4              mixer2;         //xy=594,450
+AudioMixer4              mixer3;         //xy=775,290
+AudioRecordQueue         queue1;         //xy=1089,394
+AudioOutputI2S           i2s1;           //xy=1092,315
 AudioConnection          patchCord1(playMem6, fade6);
 AudioConnection          patchCord2(playMem7, fade7);
 AudioConnection          patchCord3(playMem1, fade1);
@@ -59,11 +62,18 @@ AudioConnection          patchCord17(mixer1, 0, mixer3, 0);
 AudioConnection          patchCord18(mixer2, 0, mixer3, 1);
 AudioConnection          patchCord19(mixer3, 0, i2s1, 0);
 AudioConnection          patchCord20(mixer3, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=841,772
+AudioConnection          patchCord21(mixer3, queue1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=815,675
 // GUItool: end automatically generated code
 
-
-const int STEPS = 2048;
+byte numDigits = 1;
+byte digitPins[] = {};
+byte segmentPins[] = {35, 36, 37, 38, 39, 33, 34};
+bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
+byte hardwareConfig = COMMON_CATHODE; // See README.md for options
+bool updateWithDelays = false; // Default 'false' is Recommended
+bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
+bool disableDecPoint = true; // Use 'true' if your decimal point doesn't exist or isn't connected. Then, you only need to specify 7 segmentPins[]
 
 const int LOWC = 0;
 const int D = 1;
@@ -73,8 +83,13 @@ const int G = 4;
 const int A = 5;
 const int B = 6;
 const int C = 7;
+const int AnalogPin = 14;
+ResponsiveAnalogRead analog(AnalogPin, true);
 
 const int loader = 11;
+
+const int leftMotor = 5;
+const int rightMotor = 6;
 
 Toggle lowCSwitch(24);
 Toggle DSwitch(D + 24);
@@ -92,16 +107,67 @@ AudioPlayMemory* mems[] = {&playMem1, &playMem2, &playMem3, &playMem4, &playMem5
 AudioEffectFade* fades[] = {&fade1, &fade2, &fade3, &fade4, &fade5, &fade6, &fade7, &fade8};
 const unsigned int* samples[] = {AudioSampleC1, AudioSampleD1, AudioSampleE1, AudioSampleF1, AudioSampleG1, AudioSampleA1, AudioSampleB1, AudioSampleC2};
 
-Stepper stepper(STEPS, 3, 5, 4, 6);
-
 const long COOLDOWN = 10;
 
-long lastTreat = -1;
+unsigned long lastTreat = -1;
+
+unsigned long motorStart = 0;
 
 bool dispense = false;
 
-bool loading = false;
-
 int currSteps = 0;
+
+int currLevel = 1;
+
+int lastNote = -1;
+
+int recording = false;
+
+unsigned long lastNoteTime = 0;
+
+File currRecording;
+
+// use information from https://cran.r-project.org/web/packages/ctypesio/vignettes/wave-format.html
+void writeWavHeader() {
+  uint32_t finalSize = currRecording.size();
+
+  currRecording.seek(0);
+  byte header[44];
+
+  memcpy(&header[0], "RIFF", 4);
+  *(uint32_t*) (header + 4) = (finalSize - 8);
+  memcpy(&header[8], "WAVE", 4);
+  memcpy(&header[12], "fmt ", 4);
+  *(uint32_t*) (header + 16) = 16;
+  *(uint32_t*) (header + 20) = 1;
+  *(uint32_t*) (header + 22) = 1;
+  *(uint32_t*) (header + 24) = 44100;
+  *(uint32_t*) (header + 28) = (44100*2);
+  *(uint32_t*) (header + 32) = 2;
+  *(uint32_t*) (header + 34) = 16;
+  memcpy(&header[36], "data", 4);
+  *(uint32_t*) (header + 40) = finalSize - 44;
+
+  currRecording.write(header, 44);
+}
+
+const byte numbers[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 0, 1, 1}
+};
+
+void displayNumber(int n) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segmentPins[i], numbers[n][i]);
+  }
+}
 
 #endif
--- final_project.ino
+++ final_project.ino
@@ -2,38 +2,59 @@
 
 void setup() {
   Serial.begin(9600);
-  lastTreat = millis() - COOLDOWN;
-  stepper.setSpeed(12);
+  SD.begin(BUILTIN_SDCARD);
+  setSyncProvider(Teensy3Clock.get);
+  lastTreat = millis() - 1000*COOLDOWN;
   pinMode(13, OUTPUT);
-  AudioMemory(20);
+  AudioMemory(400);
+  queue1.begin();
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.7);
-  digitalWrite(13, HIGH);
-  for (int i = 0; i < TOTAL_SWITCHES; i++) {
-    ALL_SWITCHES[i]->begin(i+24);
+  for (int i = 0; i < 7; i++) {
+    pinMode(segmentPins[i], OUTPUT);
   }
+  pinMode(leftMotor, OUTPUT);
+  pinMode(rightMotor, OUTPUT);
+
+  digitalWrite(leftMotor, LOW);
+  digitalWrite(rightMotor, LOW);
 }
 
 void loop() {
+  if (recording && ((lastNoteTime + 10000) < millis())) {
+    recording = false;
+    addToSD();
+    writeWavHeader();
+    currRecording.close();
+    Serial.println("sd closed");
+  }
+
+  if (recording) {
+    addToSD();
+  }
+
   pollAll();
   updatePiano();
   LoaderSwitch.poll();
   if (LoaderSwitch.onPress()) {
-    loading = true;
-  }
-  if (LoaderSwitch.onRelease()) {
-    loading = false;
+    dispense = true;
+    digitalWrite(rightMotor, HIGH);
+    digitalWrite(leftMotor, LOW);
+    motorStart = millis();
   }
 
-
-  if (dispense || loading) {
-    stepper.step(-3);
-    currSteps+=3;
-    if (currSteps >= STEPS || loading) {
-      currSteps = 0;
-      dispense = false;
-    }
+  if (dispense && (motorStart + 1000 < millis())) {
+    digitalWrite(leftMotor, LOW);
+    digitalWrite(rightMotor, LOW);
+    dispense = false;
   }
+
+  analog.update();
+  int input = analog.getValue();
+  if (analog.hasChanged()) {
+    currLevel = map(input, 0, 1023, 9, 1);
+  }
+  displayNumber(currLevel);
 }
 
 void pollAll(){
@@ -52,20 +73,56 @@ void updatePiano() {
   }
 }
 
-void dispenseTreat() {
-  if (millis()-lastTreat >= 1000*COOLDOWN && !loading) {
-      lastTreat = millis();
-      Serial.println("TREAT DISPENSED!");
+void dispenseTreat(int currNote) {
+  if ((currLevel == 1) || ((lastNote + 1 == currNote) && currNote + 2 == currLevel)) {
+    lastNote = currNote;
+    if (millis()-lastTreat >= 1000*COOLDOWN && !dispense) {
+        lastTreat = millis();
+        motorStart = millis();
+        digitalWrite(rightMotor, HIGH);
+        digitalWrite(leftMotor, LOW);
+        Serial.println("TREAT DISPENSED!");
         dispense = true;
+        lastNote = -1;
+    }
+  } else {
+      if (currNote == 0) {
+        lastNote = 0;
+      } else if (lastNote + 1 == currNote) {
+        lastNote = currNote;
+      } else {
+        lastNote = -1;
+      }
   }
 }
 
 void onNote(int note) {
-  dispenseTreat();
+  dispenseTreat(note);
   mems[note]->play(samples[note]);
   fades[note]->fadeIn(1);
+  if (!recording) {
+    time_t currTime = Teensy3Clock.get();
+    String dateString = String(month(currTime)) + " " + String(day(currTime)) + ".wav";
+    currRecording = SD.open(dateString.c_str(), FILE_WRITE);
+    if (currRecording) {
+      Serial.println("sd open");
+      byte headerDummy[44] = {0};
+      currRecording.write(headerDummy, 44);
+      recording = true;
+    }
+  }
+  lastNoteTime = millis();
 }
 
 void offNote(int note) {
   fades[note]->fadeOut(5);
+}
+
+void addToSD() {
+  while (queue1.available() >= 1) {
+    byte buffer[256];
+    memcpy(buffer, queue1.readBuffer(), 256);
+    queue1.freeBuffer();
+    currRecording.write(buffer, 256);
+  }
 }
